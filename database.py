7dd05f725ff4e0fa921d9878660d2ba535966bf5
Ayobami200 <135840113+Ayobami200@users.noreply.github.com>
2026-04-14 17:09:43 +0100

    database.py

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_all, create_engine, Column, Integer, String, Float, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 import streamlit as st
